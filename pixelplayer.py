a69e334b7bb0d7da143d6e08b1cb239e32f9d955
AZZRCN <qazwsx233343@163.com>
2023-07-21 18:43:46 +0800

    Alpha

--- pixelplayer.py
+++ pixelplayer.py
@@ -1,13 +1,34 @@
 import os
 import cv2
 from rich.console import Console
+from ffpyplayer.player import MediaPlayer
+import time
 console = Console()
-for t in range(0,1001,20):
+ret = True
+#video_path = "C:\\Users\\86156\\Documents\\GitHub\\PLAYER-and-CMD-py\\AM.ts"#"强风大背头.mp4"
+video_path = input("路径:")
+video=cv2.VideoCapture(video_path)
+player = MediaPlayer(video_path)
+T1 = time.time()
+fpas = 1 / 30
+cnt = 0
+while ret:
+    #time.sleep(0.026233)
+    #console
+    for i in range((int(int(time.time() - T1) / fpas)) - cnt):
+        cnt += 1
+        video.read()
+    ret,frame = video.read()
     width, height = os.get_terminal_size().columns, os.get_terminal_size().lines
-    imagess = cv2.imread(f'{t}.jpg',-1)
-    resized_image = cv2.resize(imagess, (width,height))
+    resized_image = cv2.resize(frame, (width,height))
     os.system("cls")
     for i in resized_image:
         for j in i:
             console.print("█", style=f"rgb({j[2]},{j[1]},{j[0]})", end="")
-        console.print("")
+        #console.print("")
+    #for i in range(1,int(int(time.time()-T1) / 0.007) - cnt):
+    #    video.read()
+    #    cnt += 1
+    #    print(f"pass!{cnt}")
+    cnt += 1
+    
